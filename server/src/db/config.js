e56865a8fe2feb48be6063e6ad046675ad79ce99
CaydnPB <117025523+CaydnPB@users.noreply.github.com>
2023-11-03 11:59:14 +0000

    Update database connection and add User model

--- server/src/db/config.js
+++ server/src/db/config.js
@@ -1,10 +1,13 @@
-const { Sequelize } = require("sequelize");
+const { Sequelize, DataTypes, Model } = require("sequelize");
 const path = require("path");
 
 const db = new Sequelize({
     dialect: "sqlite",
     storage: path.join(__dirname, "db.sqlite"),
-    logging: false
+    logging: false,
+    define: {
+        timestamps: false
+    }
 });
 
-module.exports = db;
+module.exports = { db, DataTypes, Model };
--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,1 +1,11 @@
-// create your User model here
+const { db, DataTypes, Model } = require("../db/config");
+
+const User = db.define("User", {
+    username: DataTypes.STRING,
+});
+
+(async () => {
+    await db.sync();
+})();
+
+module.exports = { User };
